6697b2c020112a91912fccb079c3feaaa2ea16cd
Madison Kerndt <madison.kerndt@gmail.com>
2016-09-18 17:46:29 -0600

    filter messages

--- lib/components/Header.jsx
+++ lib/components/Header.jsx
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
 
-export default function ({ sort, reverseOrder }){
+export default function ({ sort, handleFilter, handleReverseOrder }){
   return (
     <header>
       <h1 className='header-main-title'>Shoot the Breeze</h1>
-      <input className='header-nav-input' placeholder='Filter' type='text'></input>
+      <input
+        className='header-nav-input'
+        placeholder='Filter'
+        type='text'
+        onKeyUp={ handleFilter }>
+        </input>
       <article className='header-actions'>
         <button
           disabled={!sort}
           className='header-main-sort-button-down'
-          onClick={ reverseOrder }>
+          onClick={ handleReverseOrder }>
             Sort
           </button>
         <button
           disabled={sort}
           className='header-main-sort-button-up'
-          onClick={ reverseOrder }>
+          onClick={ handleReverseOrder }>
             Sort
           </button>
       </article>
