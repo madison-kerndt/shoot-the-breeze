a97d1e13f329f048e81fe0b4f97db013b0911aaa
Madison Kerndt <madison.kerndt@gmail.com>
2016-09-18 13:39:39 -0600

    render user list

--- lib/components/UserList.jsx
+++ lib/components/UserList.jsx
@@ -3,13 +3,13 @@ import { uniqBy } from 'lodash';
 import { reference } from '../firebase';
 
 export default function ({ messages }) {
-  const UniqueUsers = uniqBy(messages, (u) => {u.user});
-  console.log(UniqueUsers);
+  let uniqueUsers = uniqBy(messages, (u) => { return u.user.uid; });
+  console.log(uniqueUsers);
   return(
     <aside className='user-list'>
       <h2 className='title-users'>Users</h2>
       <ul className='list-users'>
-        {UniqueUsers.map((m) => <li key={m.user.uid}>{ m.user.displayName } ({ m.user.email })</li>)}
+        {uniqueUsers.map((m) => <li key={m.key}>{ m.user.displayName } ({ m.user.email })</li>)}
       </ul>
     </aside>
   )
