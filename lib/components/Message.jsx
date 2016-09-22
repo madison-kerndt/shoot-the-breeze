41aae6cb2cd63441fa06ea47df8de8280b6cd663
Adam Bernard <ab255@me.com>
2016-09-22 07:39:21 -0600

    add delete function

--- lib/components/Message.jsx
+++ lib/components/Message.jsx
@@ -3,9 +3,19 @@ import moment from 'moment';
 import firebase from '../firebase';
 import SortButton from './SortButton';
 
-export default function ({ createdAt, user, content, reference }) {
-  const currentUser = firebase.auth().currentUser.email;
-  if (currentUser === user.email) {
+export default function ({ createdAt, user, content, reference, currentUser }) {
+  if (!currentUser) {
+    return (
+      <section className='message'>
+        <article className='message-time time'>
+          {moment(createdAt).format('MMMM Do, h:mm a')}
+        </article>
+        <article className='message-author'>{ user.displayName }</article>
+        <p className='message-content'>{ content }</p>
+      </section>
+    );
+  }
+  if (currentUser.uid === user.uid) {
     return (
       <section className='message'>
         <article className='message-time time'>
@@ -29,5 +39,5 @@ export default function ({ createdAt, user, content, reference }) {
       <article className='message-author'>{ user.displayName }</article>
       <p className='message-content'>{ content }</p>
     </section>
-  );
+);
 }
--- lib/components/MessageContainer.jsx
+++ lib/components/MessageContainer.jsx
@@ -3,25 +3,37 @@ import firebase from '../firebase';
 import Message from './Message';
 import { map } from 'lodash';
 
-export default function ({ messages, filtered, chosen }) {
+export default function ({ messages, filtered, chosen, currentUser }) {
   const reference = firebase.database().ref('messages');
   if (filtered.length > 0) {
     return (
       <main>
-        {filtered.map(message => <Message {...message} reference={reference.child(message.key)}/>)}
+        {filtered.map(message => <Message {...message}
+                                          reference={reference.child(message.key)}
+                                          CurrenttUser={currentUser}
+                                  />
+        )}
       </main>
     );
   }
   if (chosen.length > 0) {
     return (
       <main>
-        {chosen.map(message => <Message {...message} reference={reference.child(message.key)}/>)}
+        {chosen.map(message => <Message {...message}
+                                        reference={reference.child(message.key)}
+                                        currentUser={currentUser}
+                                        />
+        )}
       </main>
     );
   }
   return (
     <main>
-      {messages.map(message => <Message {...message} reference={reference.child(message.key)}/>)}
+      {messages.map(message => <Message {...message}
+                                        reference={reference.child(message.key)}
+                                        currentUser={currentUser}
+                                        />
+      )}
     </main>
   );
 }
